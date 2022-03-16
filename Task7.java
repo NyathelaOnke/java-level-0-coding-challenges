85c5020475dbd84dfa8dcd5aeb8c8071dbab641c
Onke Nyathela <90394041+NyathelaOnke@users.noreply.github.com>
2022-03-16 15:05:37 +0200

    Update Task7.java

--- Task7.java
+++ Task7.java
@@ -1,5 +1,4 @@
 class Task7 {
-
     static double celsiusToFahrenheit(int tempInCelsius){
         return (tempInCelsius * 1.8) + 32;
     }
@@ -9,6 +8,5 @@ class Task7 {
     public static void main(String[] args) {
         System.out.println(celsiusToFahrenheit(32));
         System.out.println(fahrenheitToCelsius(75));
-	// write your code here
     }
 }
