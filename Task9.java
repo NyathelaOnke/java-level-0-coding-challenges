ab23e92b51f5d9768a8e781fab6ed505147f10b2
Onke Nyathela <90394041+NyathelaOnke@users.noreply.github.com>
2022-03-16 15:26:37 +0200

    Update and rename Task 0.9/src/com/company/Main.java to Task9.java

--- Task9.java
+++ Task9.java
@@ -1,6 +1,4 @@
-package com.company;
-
-public class Main {
+class Task9 {
 
     static void vowelsInString(String theStr){
         theStr = theStr.toLowerCase();
@@ -15,6 +13,5 @@ public class Main {
     }
     public static void main(String[] args) {
         vowelsInString("Onke Nyathela");
-	// write your code here
     }
 }
